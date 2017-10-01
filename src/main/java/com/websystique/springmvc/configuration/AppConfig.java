0583d1953e28c5083b3ede54cbcbf9470cb678e1
sevak <sevakmart@gmail.com>
2017-10-01 11:38:10 +0400

    Swagger integrated.

--- src/main/java/com/websystique/springmvc/configuration/AppConfig.java
+++ src/main/java/com/websystique/springmvc/configuration/AppConfig.java
@@ -43,6 +43,11 @@ public class AppConfig extends WebMvcConfigurerAdapter{
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/static/**").addResourceLocations("/static/");
+
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+		registry.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 
     /**
