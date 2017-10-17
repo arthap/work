ee1881015fe07826b5455a07560aa7cd37403e78
Ruben Sharyan <Ruben_Sharyan@epam.com>
2017-10-17 11:45:34 +0400

    adding some validation in user_profile

--- src/main/java/com/websystique/springmvc/model/UserProfile.java
+++ src/main/java/com/websystique/springmvc/model/UserProfile.java
@@ -1,10 +1,12 @@
 package com.websystique.springmvc.model;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import com.websystique.springmvc.model.enums.UserProfileType;
 import org.hibernate.validator.constraints.Email;
-import org.hibernate.validator.constraints.NotEmpty;
 
 import java.util.Date;
 
@@ -16,25 +18,36 @@ public class UserProfile {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
-    @NotEmpty
+    @NotNull
+    @Pattern(regexp = "[A-Z,a-z,0-9]{8,50}",message = "please insert valid password, password should be latter and number, length is at least 9 most 50 ")
     @Column(name = "password", nullable = false)
     private String password;
 
-    @NotEmpty
+    @NotNull
+    @Size(min = 2, max = 50, message = "please insert valid firstName, length is at least 2 most 50")
     @Column(name = "first_name", nullable = false)
     private String firstName;
 
-    @NotEmpty
+    @NotNull
+    @Size(min = 2, max = 50,message = "please insert valid lastName, length is at least 2 most 50")
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @NotEmpty
+    @NotNull
+    @Pattern(regexp = "[0-9]{16,99}",message = "please insert valid age,should be only number and between of 16-99 ")
+    @Column(name = "age", nullable = false)
+    private int age;
+
+    @NotNull
+    @Email
+    @Pattern(regexp = "[A-Za-z0-9._%-+]+@[A-Za-z0-9.-]+\\.[A-Za-z]{8,50}",message = "please insert valid email")
     @Column(name = "email", nullable = false)
     private String email;
 
     @Column(name = "avatar", nullable = false)
     private String profileImagePath;
 
+    @NotNull
     @Temporal(TemporalType.DATE)
     @Column(name = "created_at", nullable = false)
     private Date createdAt;
@@ -76,6 +89,14 @@ public class UserProfile {
         this.lastName = lastName;
     }
 
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
     public String getEmail() {
         return email;
     }
@@ -107,5 +128,36 @@ public class UserProfile {
 
     public void setCreatedAt(Date createdAt) {
         this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return "UserProfile{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                ", email='" + email + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof UserProfile)) return false;
+
+        UserProfile that = (UserProfile) o;
+
+        if (getId() != null ? !getId().equals(that.getId()) : that.getId() != null) return false;
+        return getEmail() != null ? getEmail().equals(that.getEmail()) : that.getEmail() == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = getId() != null ? getId().hashCode() : 0;
+        result = 31 * result + (getEmail() != null ? getEmail().hashCode() : 0);
+        return result;
     }
 }
